be6423fc03a18977ce14f2de149010cca2ef9ab4
Johan Dewe <johan@dewe.net>
2013-09-10 14:02:31 +0200

    Added a nancy host and module.
    Self hosting on port 1337. Simplest posslible module. Really SDHP as promised.

---
+++ Toggliatelle.Web/App_Code/Toggliatelle/Api/ToggleModule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+
+namespace Toggliatelle.Api
+{
+    public class ToggleModule : Nancy.NancyModule
+    {
+        public ToggleModule()
+        {
+            Get["/"] = _ => "Toggliatelle root";
+        }
+    }
+}
--- Toggliatelle.Web/Global.asax.cs
+++ Toggliatelle.Web/Global.asax.cs
@@ -6,6 +6,7 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Nancy.Hosting.Self;
 
 namespace Toggliatelle.Web
 {
@@ -14,14 +15,29 @@ namespace Toggliatelle.Web
 
     public class MvcApplication : System.Web.HttpApplication
     {
+        NancyHost nancyHost;
+
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
 
-            WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            StartToggliatelle();
+        }
+
+        private void StartToggliatelle()
+        {
+            // Create namespace reservations manually with the (elevated) command(s):
+            // netsh http add urlacl url=http://+:1337/ user=Everyone"}
+            // See http://msdn.microsoft.com/en-us/library/ms733768.aspx for more information.
+
+            nancyHost = new NancyHost(new Uri("http://localhost:1337"));
+            nancyHost.Start();
+
+
         }
     }
 }
