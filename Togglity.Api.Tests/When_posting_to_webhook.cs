8799e1d72224b143573dcfe55f7c4678472bce83
Johan Dewe <johan.dewe@fritidsresor.se>
2013-11-08 07:28:34 +0100

    Added web api project

---
+++ Togglity.Api.Tests/When_posting_to_webhook.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Togglity.Api.Tests
+{
+    [TestFixture]
+    public class When_posting_to_webhook
+    {
+        [Test]
+        public void It_should_get_toggles_from_server()
+        {
+
+        }
+
+    }
+}
---
+++ Togglity.Api/Controllers/TogglesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Togglity.Api.Controllers
+{
+    public class TogglesController : ApiController
+    {
+        // POST toggles/webhook
+        [HttpPost]
+        [Route("toggles/webhook")]
+        public void Post([FromBody]string value)
+        {
+
+        }
+    }
+}
