c66c2f442539e2ca80fcf057763c3ec47afc36ae
Johan Dewe <johan@dewe.net>
2013-12-11 23:05:44 +0100

    Model, take one

---
+++ source/Togglity.Server/Controllers/TogglesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Togglity.Server.Models;
+
+namespace Togglity.Server.Controllers
+{
+    public class TogglesController : ApiController
+    {
+        // GET api/toggles
+        public Toggles Get()
+        {
+            return new Toggles
+            {
+                new Feature {Name = "Toggle1", Enabled = true}
+            };
+        }
+    }
+}
---
+++ source/Togglity.Server/Models/Feature.cs
@@ -0,0 +1,8 @@
+﻿namespace Togglity.Server.Models
+{
+    public class Feature
+    {
+        public string Name { get; set; }
+        public bool Enabled { get; set; }
+    }
+}
